8376fa160eca7c6363fb83640ed4b0a95eeb1034
K3 <anand21nanda@gmail.com>
2015-01-12 15:36:47 +0530

    python - custom objects in python set

---
+++ python/exercise/custom_objects_set.py
@@ -0,0 +1,26 @@
+class Person(object):
+    def __init__(self, name, age):
+        self.name, self.age = name, age
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__) and
+            getattr(other, 'name', None) == self.name and
+            getattr(other, 'age', None) == self.age)
+
+    def __hash__(self):
+        return hash(self.name + str(self.age))
+
+
+class Person2(object):
+    def __init__(self, name, age):
+        self.name, self.age = name, age
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__) and
+            getattr(other, 'name', None) == self.name and
+            getattr(other, 'age', None) == self.age)
+
+
+class Person3(object):
+    def __init__(self, name, age):
+        self.name, self.age = name, age
