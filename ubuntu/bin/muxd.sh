32a1318673244b52345082802fdde999e441c311
chillaranand <anand21nanda@gmail.com>
2016-10-03 14:05:08 +0530

    Add muxd script

---
+++ ubuntu/bin/muxd.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+
+cd /home/moriarty/packages/
+git clone https://github.com/libimobiledevice/libimobiledevice
+git clone https://github.com/libimobiledevice/libplist
+git clone https://github.com/libusb/libusb
+git clone https://github.com/libimobiledevice/usbmuxd
+
+sudo apt-get remove --purge -y libimobiledevice-dev
+sudo apt-get remove --purge -y libplist-dev
+sudo apt-get remove --purge -y libusb-dev
+
+
+
+cd /tmp
+cd /home/moriarty/packages/
+
+cd libplist
+./autogen.sh
+make
+sudo make install
+
+
+cd /tmp
+cd /home/moriarty/packages/
+
+cd libusb
+./autogen.sh
+make
+sudo make install
+
+
+cd /tmp
+cd /home/moriarty/packages/
+
+cd libimobiledevice
+./autogen.sh
+make
+sudo make install
+
+
+cd /tmp
+cd /home/moriarty/packages/
+
+cd usbmuxd
+./autogen.sh
+make
+sudo make install
